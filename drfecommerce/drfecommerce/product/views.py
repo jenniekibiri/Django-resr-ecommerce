1dcc944c29e9613a79c7cc00a3376319b2498ad6
jenniekibiri <josphatje@gmail.com>
2023-02-10 21:15:45 +0300

    document using spectacular and swagger

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.response  import Response
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
 
-class CatergoryView(viewsets.ViewSet):
-    """a view set for viewing all categories"""
 
+class CategoryViewSet(viewsets.ViewSet):
+    """Category ViewSet"""
     queryset = Category.objects.all()
-
-def list (self, request):
-    """list all categories"""
-    serializer = CategorySerializer(self.queryset, many=True)
-    return Response(serializer.data)
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
