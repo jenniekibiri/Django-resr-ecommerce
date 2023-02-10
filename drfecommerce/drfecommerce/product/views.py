fa6f6dece40bd5e0f58bbc332175a89048d5f3c7
jenniekibiri <josphatje@gmail.com>
2023-02-10 14:22:00 +0300

    serialize category

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response  import Response
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
-# Create your views here.
+
+class CatergoryView(viewsets.ViewSet):
+    """a view set for viewing all categories"""
+
+    queryset = Category.objects.all()
+
+def list (self, request):
+    """list all categories"""
+    serializer = CategorySerializer(self.queryset, many=True)
+    return Response(serializer.data)
