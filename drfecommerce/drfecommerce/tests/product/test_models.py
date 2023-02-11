dbc79aa41eeb9c1f61e8b83d9885dc39555d49b5
jenniekibiri <josphatje@gmail.com>
2023-02-11 21:52:15 +0300

    write tests for category, products and brand

---
+++ drfecommerce/drfecommerce/tests/product/test_models.py
@@ -0,0 +1,34 @@
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCategoryModel:
+    def test_str_method(self, category_factory):
+        data = category_factory()
+        assert data.__str__() == "test_category"
+
+class TestBrandModel:
+    def test_str_method(sel, brand_factory):
+        data = brand_factory()
+        assert data.__str__( )=="test_brand"
+
+class TestProductModel:
+    def test_str_method(sel, product_factory, brand_factory, category_factory):
+        data = product_factory(
+            name="test_product1",
+            description="test_description",
+            is_digital=False,
+            brand=brand_factory( 
+                name="test_brand",
+            ),
+            category=category_factory(
+                name="test_category",
+            ),
+
+        )
+        assert data.__str__()=="test_product1"
+        assert data.brand.__str__()=="test_brand"
+        assert data.category.__str__()=="test_category"
+        assert data.description=="test_description"
+      
