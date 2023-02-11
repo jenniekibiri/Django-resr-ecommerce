6545bc7ee7971eef5c9775da988be9c7795cb383
jenniekibiri <josphatje@gmail.com>
2023-02-11 17:51:46 +0300

    create product view set create brand viewset

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -10,7 +10,27 @@ from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 class CategoryViewSet(viewsets.ViewSet):
     """Category ViewSet"""
     queryset = Category.objects.all()
+
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+class BrandViewSet(viewsets.ViewSet):
+    """Brand ViewSet"""
+    queryset = Brand.objects.all()
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+
+
+
+class ProductViewSet (viewsets.ViewSet):
+    """Product ViewSet"""
+    queryset = Product.objects.all()
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
